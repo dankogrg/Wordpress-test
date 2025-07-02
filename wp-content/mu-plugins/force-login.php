626776d4e88ca26085f7df924519dd2b9e8cbe6c
dankogrg <148497517+dankogrg@users.noreply.github.com>
2025-07-02 09:12:34 +0000

    resolved SITEURL broken port forwarding issue

--- wp-content/mu-plugins/force-login.php
+++ wp-content/mu-plugins/force-login.php
@@ -1,21 +1,21 @@
 <?php
-add_action('init', function() {
-    $request_uri = $_SERVER['REQUEST_URI'] ?? '';
-    // If not logged in and not on login/admin, redirect to login
-    if (
-        !is_user_logged_in() &&
-        strpos($request_uri, '/wp-login.php') !== 0 &&
-        strpos($request_uri, '/wp-admin') !== 0
-    ) {
-        wp_redirect(wp_login_url() . '?loggedout=true&wp_lang=en_US');
-        exit;
-    }
-    // If logged in and on root, redirect to admin dashboard
-    if (
-        is_user_logged_in() &&
-        ($request_uri === '/' || $request_uri === '') // root URL
-    ) {
-        wp_redirect(admin_url());
-        exit;
-    }
-});
+// add_action('init', function() {
+//     $request_uri = $_SERVER['REQUEST_URI'] ?? '';
+//     // If not logged in and not on login/admin, redirect to login
+//     if (
+//         !is_user_logged_in() &&
+//         strpos($request_uri, '/wp-login.php') !== 0 &&
+//         strpos($request_uri, '/wp-admin') !== 0
+//     ) {
+//         wp_redirect(wp_login_url() . '?loggedout=true&wp_lang=en_US');
+//         exit;
+//     }
+//     // If logged in and on root, redirect to admin dashboard
+//     if (
+//         is_user_logged_in() &&
+//         ($request_uri === '/' || $request_uri === '') // root URL
+//     ) {
+//         wp_redirect(admin_url());
+//         exit;
+//     }
+// });
